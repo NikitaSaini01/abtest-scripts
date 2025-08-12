a634fece6624eeef3be7596218eebe83de719627
NikitaSaini01 <Nikita.Saini01@eclerx.com>
2025-08-12 15:32:59 +0530

    Updates

--- merged.js
+++ merged.js
@@ -8,23 +8,6 @@
 `;
   document.head.appendChild(style);
 
-  // Inject HTML
-  const container = document.createElement('div');
-  container.innerHTML = `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <h1 id="first-tag">Click Me</h1>
-    <script src="script.js"></script>
-</body>
-</html>`;
-  document.body.appendChild(container);
-
   // Run JS
   (function(){
 document.getElementById("first-tag").onclick = () => {
